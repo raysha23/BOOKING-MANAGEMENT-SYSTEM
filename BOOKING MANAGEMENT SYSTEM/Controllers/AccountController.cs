8f003d5f242b3709cbf04692b9e40d1baeb78f19
raysha23 <sismundoraymund@gmail.com>
2026-01-01 22:45:55 +0800

    Login Form

--- BOOKING MANAGEMENT SYSTEM/Controllers/AccountController.cs
+++ BOOKING MANAGEMENT SYSTEM/Controllers/AccountController.cs
@@ -4,7 +4,7 @@ namespace BOOKING_MANAGEMENT_SYSTEM.Controllers
 {
   public class AccountController : Controller
   {
-    public IActionResult Index()
+    public IActionResult Login()
     {
       return View();
     }
